444050b023df491349f3f19aea906d5a813a8e6f
sangnq <sangnq@vuthao.com>
2023-11-27 16:36:23 +0700

    add project react and root project vna to review

--- lib/main.dart
+++ lib/main.dart
@@ -5,10 +5,11 @@ import 'package:vna_dcm_flutter/src/repositories/apis/DioSingleton.dart';
 import 'package:vna_dcm_flutter/src/utils/Constant.dart';
 import 'package:vna_dcm_flutter/src/views/login/LoginScreen.dart';
 import 'package:dio/dio.dart';
+import 'package:vna_dcm_flutter/src/widgets/LoadingWidget.dart';
 
 Future<void> main() async {
   // gọi để lấy session
   await Constant.client.get(
       "https://vnadmsuatportal.vuthao.com/psd/api/ApiMobile.ashx?func=AdfsLogin");
-  runApp(MaterialApp(home: LoginScreen(),));
+  runApp(MaterialApp(home: LoadingWidget(),));
 }
--- lib/src/views/login/LoginScreen.dart
+++ lib/src/views/login/LoginScreen.dart
@@ -60,7 +60,6 @@ class LoginScreen extends StatelessWidget {
           ),
         )
       ),
-
     );
   }
 }
