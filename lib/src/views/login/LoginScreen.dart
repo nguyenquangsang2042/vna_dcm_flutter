603716437efdf6b3a9de808e168ca9212d001491
sangnq <sangnq@vuthao.com>
2023-11-27 16:00:52 +0700

    update login screen

--- lib/src/views/login/LoginScreen.dart
+++ lib/src/views/login/LoginScreen.dart
@@ -1,10 +1,66 @@
 import 'package:flutter/material.dart';
 
 class LoginScreen extends StatelessWidget {
-  const LoginScreen({super.key});
-
+  LoginScreen({super.key});
+  final TextEditingController _usernameController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('lib/src/assets/images/background_main.png'), // Replace with your image path
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: Container(
+          padding: EdgeInsets.all(10),
+          child:  Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Container(
+                  height: 50,
+                  child: TextField(
+                    controller: _usernameController,
+                    decoration: InputDecoration(
+                      labelText: 'Tài khoản',
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10.0), // Adjust the value as needed
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(height: 15),
+                Container(
+                  height: 50,
+                  child: TextField(
+                    controller: _passwordController,
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10.0), // Adjust the value as needed
+                      ),
+                      labelText: 'Mật khẩu',
+                    ),
+                    obscureText: true,
+                  ),
+                ),
+                SizedBox(height: 10),
+                Container(
+                  width: MediaQuery.of(context).size.width,
+                  height: 40,
+                  child:  ElevatedButton(
+                    onPressed: () {},
+                    child: Text('Đăng nhập'),
+                  ),
+                )
+              ],
+            ),
+          ),
+        )
+      ),
+
+    );
   }
 }
