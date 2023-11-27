80eab5c902627e6aef7e2f7ff46e239611dc57f6
sangnq <sangnq@vuthao.com>
2023-11-27 17:17:30 +0700

    add Loading Widget

--- lib/src/widgets/LoadingWidget.dart
+++ lib/src/widgets/LoadingWidget.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 
 class LoadingWidget extends StatelessWidget {
   const LoadingWidget({super.key});
@@ -6,8 +7,28 @@ class LoadingWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-
+      body: Stack(
+        children: [
+          Container(
+            decoration: const BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage('lib/src/assets/images/background_main.png'), // Replace with your image path
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+          const Positioned(
+            left: 0,
+            right: 0,
+            bottom: 20, // Adjust the bottom value as needed
+            child: Center(
+              child: SpinKitSpinningLines(
+                color: Colors.blue, // Set the color of the spinner
+                size: 50.0, // Set the size of the spinner
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
