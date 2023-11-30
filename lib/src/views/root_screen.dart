abbba795215bd521232a38e572690583fd71cf2e
sang <nguyenquangsang2042@gmail.com>
2023-11-30 20:40:36 +0700

    show appbar

--- lib/src/views/root_screen.dart
+++ lib/src/views/root_screen.dart
@@ -1,3 +1,4 @@
+import 'package:bottom_sheet_scaffold/bottom_sheet_scaffold.dart';
 import 'package:cherry_toast/cherry_toast.dart';
 import 'package:cherry_toast/resources/arrays.dart';
 import 'package:flutter/material.dart';
@@ -15,7 +16,35 @@ class RootScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final loginBloc = BlocProvider.of<LoginBloc>(context);
-    return Scaffold(
+    return BottomSheetScaffold(
+      appBar: PreferredSize(
+        preferredSize: Size.fromHeight(50.0), // Set the height as needed
+        child: BlocBuilder<LoginBloc, LoginState>(
+          builder: (context, state) {
+            if (state is LoginSuccess) {
+              return AppBar(
+                title: Text("widget.title"),
+                leading: InkWell(
+                  onTap: () {
+                    if (BottomSheetPanel.isOpen) {
+                      BottomSheetPanel.close();
+                    } else {
+                      BottomSheetPanel.open();
+                    }
+                  },
+                  child: const Icon(
+                    Icons.arrow_back_ios,
+                    color: Colors.black54,
+                  ),
+                ),
+              );
+            }
+            return const SizedBox(
+              height: 0,
+            );
+          },
+        ),
+      ),
       body: BlocBuilder<LoginBloc, LoginState>(
         builder: (context, state) {
           if (state is LoginLoading) {
@@ -48,6 +77,14 @@ class RootScreen extends StatelessWidget {
             return LoginScreen();
           }
         },
+      ),
+      draggableBody: true,
+      dismissOnClick: true,
+      barrierColor: Colors.black54,
+      bottomSheet: DraggableBottomSheet(
+        animationDuration: Duration(milliseconds: 200),
+        body: Container(),
+        header: Container(), //header is not required
       ),
     );
   }
